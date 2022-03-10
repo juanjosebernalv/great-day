b5a31f1b62f79e2c273f81ddaa41e7fb270d4c04
Juan Jose Bernal Villamarin <juanjosebernalvillamarin@gmail.com>
2022-03-09 21:54:57 -0500

    Load local Data

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import logo from './logo.svg';
+import styled from "styled-components";
 import './App.css';
 import MyButtonComponent from './components/MyButtonComponent';
 import MyButtonStyled from './components/MyButtonStyled';
 import MyCard from './components/MyCard';
 import NotificationCard from './components/NotificationCard';
+
+import { menuData } from "./data/menuData";
+import NavigationButton from "./components/NavigationButton";
 
 function App() {
   return (
@@ -13,6 +17,14 @@ function App() {
           Hello World - Juan Jose.🏍️
         </p> */}
         {/* <MyButtonComponent></MyButtonComponent> */}
+        <Wrapper>
+            <ButtonsWrapper>
+                {menuData.map((data) => {
+                    return <NavigationButton image={data.image} title={data.title} />;
+                })}
+            </ButtonsWrapper>
+        </Wrapper>
+
         <MyButtonStyled></MyButtonStyled>
         <MyCard></MyCard>
         <NotificationCard></NotificationCard>
@@ -22,3 +34,15 @@ function App() {
 }
 
 export default App;
+
+
+const Wrapper = styled.div`
+    padding-top: 150px;
+    margin: 0 auto;
+`;
+
+const ButtonsWrapper = styled.div`
+    display: flex;
+    justify-items: space-between;
+    justify-content: center;
+`;
